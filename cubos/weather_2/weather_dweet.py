361ddf1ae05cb1bd0069bb4423d239182acab65e
Felipe González <felipexgonzalez@gmail.com>
2015-06-18 12:21:52 -0500

    Agregar reportes a wunderground

--- cubos/weather_2/weather_dweet.py
+++ cubos/weather_2/weather_dweet.py
@@ -5,22 +5,27 @@ import json
 import phant
 import logging
 import logging.handlers
-
+import math
 ## logging
 format_logging = logging.Formatter(fmt='%(levelname)s|%(asctime)s|%(name)s| %(message)s ', datefmt="%Y-%m-%d %H:%M:%S")
 h = logging.handlers.TimedRotatingFileHandler('/Volumes/mmshared/bdatos/log/monitor/estacion_meteo.log', encoding='utf8',
-        interval=1, when='D', backupCount=1)
+        interval=1, when='midnight', backupCount=4000)
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.DEBUG)
 h.setFormatter(format_logging)
 h.setLevel(logging.DEBUG)
 root_logger.addHandler(h)
 
-p = phant.Phant("XGvXXOnn1aFKYpJGvV6r", 'temperature','humidity',
-	'wind_direction','wind_speed','rain_mm_day',private_key = "1J0ggV88qKtMbqDnN64x")
+url_wunder = 'http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?'
+id_wunder = 'IDISTRIT49' 
+pass_wunder= 'A34-Qg6-272-6Jq'
+#http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?ID=KCASANFR5&PASSWORD=XXXXXX&dateutc=2000-01-01+10%3A32%3A35&winddir=230&windspeedmph=12&windgustmph=12&tempf=70&rainin=0&baromin=29.1&dewptf=68.2&humidity=90&weather=&clouds=&softwaretype=vws%20versionxx&action=updateraw
+url_1 = url_wunder + 'ID='+id_wunder+'&PASSWORD='+pass_wunder
+#p = phant.Phant("XGvXXOnn1aFKYpJGvV6r", 'temperature','humidity',
+#	'wind_direction','wind_speed','rain_mm_day',private_key = "1J0ggV88qKtMbqDnN64x")
 while(True):
 	try:
-		r = requests.get('http://estacionyun.local/arduino/weather/0')
+		r = requests.get('http://estacionyun.local/arduino/weather/0', timeout=20)
 		print('Dweeting')
 		print(r.text.strip("\r\n"))
 		logging.info('Estacion:'+r.text.strip("\r\n"))
@@ -32,11 +37,24 @@ while(True):
 			print("error dweepy")
 			logging.error('Error dweepy')
 
-		#try:
+		#weather underground
+		try:
+			rh = float(d['humidity'])
+			tc = float(d['temperature'])
+			h = (math.log10(rh) -2.0)/0.4343 + (17.62*tc)/(243.12+tc)
+			dp = 243.12*h/(17.62-h)
+			dewpointf=dp*(9.0/5.0) +32.0
+			drainin=(float(d['rain_mm_day'])/10.0)/2.54
+			rainin=(float(d['rain_mm_hour'])/10.0)/2.54
+			url_wu = url_1 + '&dateutc=now&tempf='+str(float(d['temperature'])*(9.0/5.0)+32.0) +'&humidity='+ d['humidity'] +'&dailyrainin='+str(drainin)+'&rainin='+str(rainin)+'&windspeedmph='+str(float(d['wind_speed'])/1.60934)+'&winddir='+d['wind_direction']
+			url_wu_2 = url_wu + '&dewpointf='+str(round(dewpointf,3))
+			print(url_wu_2)
+			q = requests.get(url_wu, timeout=10)
+			print(q.status_code)
 			#p.log(d['temperature'],d['humidity'],d['wind_direction'],d['wind_speed'],d['rain_mm_day'])
 			#print(p.remaining_bytes, p.cap)
-		#except:
-			#print("Error sparkfun.")
+		except:
+			print("Error wunderground.")
 	except:
 		print("Error request--")
 		logging.error('Error request')
