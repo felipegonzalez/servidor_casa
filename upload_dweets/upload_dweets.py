5a4d09ca20d00e80cdac57a30c1251fbf191bdf6
Felipe González <felipexgonzalez@gmail.com>
2015-07-12 15:24:43 -0500

    Cambiar base de dweets a cola de redis

--- upload_dweets/upload_dweets.py
+++ upload_dweets/upload_dweets.py
@@ -4,31 +4,42 @@ import dweepy
 import cPickle
 import sqlite3 as lite
 import time
-
+import redis
 con_dweet= lite.connect('/Volumes/mmshared/bdatos/to_dweet.db')
-
+redis_q = redis.Redis()
 
 def uploadDweet():
     time_d = time.time()
 
     while(True):
         if(time.time()-time_d > 20):
-            print "Sending data"
+            print "Sending data, " + str(time.time())
             time_d = time.time()
-            with con_dweet:
-                c = con_dweet.cursor()
-                c.execute('SELECT * FROM dweets')
-                actuales = c.fetchall()
+            while (redis_q.llen('queue') > 0):
+                print "Queue len: %s" % redis_q.llen('queue')
+                message = redis_q.blpop('queue')
+                message_d = cPickle.loads(message[1])
+                thing = message_d[0]
+                data = message_d[1]
+                try:
+                    #pass
+                    dweepy.dweet_for(thing, data)
+                except:
+                    print "Error dweepy"
+            #with con_dweet:
+            #    c = con_dweet.cursor()
+            #    c.execute('SELECT * FROM dweets')
+            #    actuales = c.fetchall()
                 #file = open('/Volumes/mmshared/bdatos/log/dweets_'+time.strftime("%d-%m-%Y")+'.dat', 'a+')
-                for dweets in actuales:
-                    thing = dweets[0]
-                    data = cPickle.loads(str(dweets[1]))
-                    try:
+            #    for dweets in actuales:
+            #        thing = dweets[0]
+            #        data = cPickle.loads(str(dweets[1]))
+            #        try:
                         #file.write(time.strftime("%d-%m-%Y %H:%M:%S")+','+thing+','+json.dumps(data)+'\n')
-                        dweepy.dweet_for(thing, data)
-                    except:
-                        print "Error subiendo dweets"
-                c.execute('DELETE FROM dweets')
+            #            dweepy.dweet_for(thing, data)
+            #        except:
+            #            print "Error subiendo dweets"
+            #    c.execute('DELETE FROM dweets')
                 #file.close()
 
 
@@ -37,6 +48,6 @@ if __name__ == "__main__":
         uploadDweet()
     except KeyboardInterrupt:
         #GPIO.output("P8_10", GPIO.LOW)    
-        con_dweet.close()
+        #con_dweet.close()
         #conrds.close()
         pass
