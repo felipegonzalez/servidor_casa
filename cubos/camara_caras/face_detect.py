bda96f1d4e12540bcf0ac3c8eddd986cba91300b
Felipe Gonzalez <felipexgonzalez@gmail.com>
2015-08-13 15:16:51 -0500

    Reporte más lento de consumo y script de detección de caras

---
+++ cubos/camara_caras/face_detect.py
@@ -0,0 +1,35 @@
+import cv2
+import sys
+import time
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+    print "Hay %s caras" %len(faces)
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        face_out = gray[y:y+h, x:x+w]
+        print x,y,w,h
+        cv2.imwrite('faces/face_'+str(time.time())+'.jpg', face_out)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
--- cubos/monitor_ted/monitor_ted.ino
+++ cubos/monitor_ted/monitor_ted.ino
@@ -7,67 +7,81 @@
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
-
-
-
+unsigned long time_start;
+unsigned long interval = 180000;
+int primera = 1;
 #include <EmonLib.h>
 EnergyMonitor emon;
 
 void setup() {
+  
   // put your setup code here, to run once:
   //analogReference(EXTERNAL);
   Serial.begin(9600);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
    delay(2000);
   display.clearDisplay();
-    display.setTextSize(2);
+    display.setTextSize(1);
    display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.println("Setting up...");
+  display.println("Arrancando...");
   display.display();
 
 //  emon.current(1, 111.1);
   emon.current(1, 151.0);
+  emon.voltage(2, 0.95*234.26/2.0, 1.7);
   delay(2000);
-  for(int i=0; i <10; i++){
-      double testIrms = emon.calcIrms(1480);
-     delay(200);   
-  }
+  //for(int i=0; i <2; i++){
+   //   double testIrms = emon.calcIrms(1480);
+   //  delay(200);   
+  //}
+  time_start = millis();
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  double Irms = emon.calcIrms(1480);
-  double w_apparent = 120*Irms;
+  //double Irms = emon.calcIrms(1480);
+  emon.calcVI(40,5000); 
+  float realPower       = emon.realPower;        //extract Real Power into variable
+  float apparentPower   = emon.apparentPower;    //extract Apparent Power into variable
+  float powerFactor     = emon.powerFactor;      //extract Power Factor into Variable
+  float supplyVoltage   = emon.Vrms;             //extract Vrms into Variable
+  float Irms            = emon.Irms;             //extract Irms into Variable
+  //double w_apparent = 120*Irms;
   
   //Serial.println(analogRead(1));
 
   display.clearDisplay();
   display.setCursor(0,0);
+  display.print("Voltios: ");
+  display.println(supplyVoltage);
   display.print("Amps: ");
   display.println(Irms);
-    display.print("Watts: ");
-  display.println(w_apparent);
+  display.println(" ");
+  display.print("Potencia real: ");
+  display.println(realPower);
+  display.print("Fctr potencia: ");
+  display.println(powerFactor);
+  //display.print("Amps: ");
+  //display.println(Irms);
+  //  display.print("Watts: ");
+  //display.println(w_apparent);
+  unsigned long time_now = millis();
+  //Serial.println(time_now-time_start);
+  //display.println(time_now-time_start);
+  if((time_now - time_start) > interval || primera == 0){
+    primera=0;
+    display.println("Transmitiendo...");
+    Serial.print("ct,A,1,");
+    Serial.println(Irms);
+    Serial.print("ct,kW,1,");
+    Serial.println(realPower/1000.0); 
+  }
+  //emon.serialprint();
   
-  Serial.print("ct,A,1,");
-  Serial.println(Irms);
-  Serial.print("ct,W,1,");
-  Serial.println(w_apparent);
-  
-   display.println("Sending...");
+  //display.println("Sending...");
   display.display();
-  
-  delay(5000);
-  
-  
-
+  delay(7000);
 
 }
